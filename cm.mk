f4b94c37c99825be01c1eb6765473e0c32340621
Ethan Chen <intervigil@gmail.com>
2013-10-28 18:00:03 -0700

    m7ul: remove GSM APN list
    Change-Id: I3f9b41c73c8ad0664714f65df50ac34e74680e00

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, device/htc/m7ul/full_m7ul.mk)
 
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
