3b3ec2afcb666afcbbc9e83b25ac6d4be6fa4223
blunden <blunden2@gmail.com>
2013-11-28 00:01:54 +0100

    m7_ul: Update the fingerprint to match the 4.4 GE OTA
    Change-Id: I5153f9b5dbd73a8f8946a8b4e06ade3f2deafc20

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66V.H10 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=KRT16S.H5 BUILD_FINGERPRINT="htc/m7_google/m7:4.4/KRT16S.H5/280886:user/release-keys" PRIVATE_BUILD_DESC="3.58.1700.5 CL280886 release-keys"
 
 PRODUCT_NAME := cm_m7ul
 PRODUCT_DEVICE := m7ul
