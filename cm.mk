b9925c93edb25ae5fc7d6cb90181be90fc3b2a63
Craig Schmitt <craigschmitt@cmshome.net>
2014-01-21 15:03:28 -0600

    m7: Update the fingerprint to match the 4.4.2 GE OTA
    Change-Id: I9c34033a7649c7d0fdad1a3cd7d261d35691af20

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=KRT16S.H5 BUILD_FINGERPRINT="htc/m7_google/m7:4.4/KRT16S.H5/280886:user/release-keys" PRIVATE_BUILD_DESC="3.58.1700.5 CL280886 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=KOT49H.H1 BUILD_FINGERPRINT="htc/m7_google/m7:4.4.2/KOT49H.H1/288144:user/release-keys" PRIVATE_BUILD_DESC="3.62.1700.1 CL288144 release-keys"
 
 PRODUCT_NAME := cm_m7
 PRODUCT_DEVICE := m7
