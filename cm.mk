fe3758af0f619ba8de94de5dd207cb242e4c41c5
Ethan Chen <intervigil@gmail.com>
2014-06-17 00:39:49 -0700

    m7: Update build fingerprint
    Change-Id: I46175caedf190a933d2e2707db06409aa8c57e94

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7/full_m7.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=KOT49H.H1 BUILD_FINGERPRINT="htc/m7_google/m7:4.4.2/KOT49H.H1/288144:user/release-keys" PRIVATE_BUILD_DESC="3.62.1700.1 CL288144 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=KTU84L.H4 BUILD_FINGERPRINT="htc/m7_google/m7:4.4.3/KTU84L.H4/347097:user/release-keys" PRIVATE_BUILD_DESC="4.07.1700.4 CL347097 release-keys"
 
 PRODUCT_NAME := cm_m7
 PRODUCT_DEVICE := m7
