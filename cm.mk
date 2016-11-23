6b709de3202b40b1d4ee81b31fd6e062260dd92e
David Hays <dhays90@gmail.com>
2016-11-23 08:24:05 -0600

    m7: Remove enhanced NFC feature
    Change-Id: I7c38a3281965d81f660bcbb0941f5eecbb9bc848

--- cm.mk
+++ cm.mk
@@ -17,9 +17,6 @@ $(call inherit-product, device/htc/m7/full_m7.mk)
 # Inherit CM full phone configuration
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Device naming
 PRODUCT_NAME := cm_m7
 
